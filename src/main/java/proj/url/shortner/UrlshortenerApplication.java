806ec85720e46df241ffb05f6e637d0f9ec0dd1a
parascoder <parascoder@gmail.com>
2018-12-20 00:42:13 +0530

    initial commit

---
+++ src/main/java/proj/url/shortner/UrlshortenerApplication.java
@@ -0,0 +1,14 @@
+package proj.url.shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerApplication.class, args);
+	}
+
+}
+
